7c185b8c3e3020c9fe46c44f970b3ce28bed5985
Fernando Naranjo <fnaranjob@supracnc.com>
2020-05-30 21:18:41 -0500

    refactor: Order members to match constructor

--- board.h
+++ board.h
@@ -7,17 +7,18 @@
 class Board
 {
 private:
-    const char kBorderChar_ = 'X';
-    const std::string kBorderColor_;
+
     size_t width_;
     size_t height_;
     std::vector<char> content_;
     std::pair<size_t,size_t> food_pos_;
+    const char kBorderChar_ = 'X';
+    const std::string kBorderColor_;
 
     //Snake s;
 
 public:
-    Board(size_t width_, size_t height_, std::string bc=color_codes::kBlue);
+    Board(size_t w, size_t h, std::string bc=color_codes::kBlue);
     void display() const;
     void reset();
 };
