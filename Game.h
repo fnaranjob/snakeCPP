0a832fb850c8df3d05d8e73a7e2aaa128eb57c1d
Fernando Naranjo B <fnaranjob@supracnc.com>
2020-06-15 00:21:28 -0500

    feat: Add movement controls

--- Game.h
+++ Game.h
@@ -9,8 +9,9 @@ class Game : public olcConsoleGameEngine
 private:
 	Board board_;
 	float update_time_;
+	Direction current_dir_;
 public:
-	Game(size_t width, size_t height): board_(width,height), update_time_{ 0.0 } {
+	Game(size_t width, size_t height) : board_(width, height), update_time_{ 0.0 }, current_dir_{ Direction::NONE } {
 		ConstructConsole(width, height, 10, 10);
 	};
 	void DrawBoard_(){
@@ -27,8 +28,17 @@ public:
 	}
 	virtual bool OnUserUpdate(float fEnlapsedTime) {
 		update_time_ += fEnlapsedTime;
+		if (m_keys[VK_UP].bHeld)
+			current_dir_ = Direction::UP;
+		else if (m_keys[VK_LEFT].bHeld)
+			current_dir_ = Direction::LEFT;
+		else if (m_keys[VK_RIGHT].bHeld)
+			current_dir_ = Direction::RIGHT;
+		else if (m_keys[VK_DOWN].bHeld)
+			current_dir_ = Direction::DOWN;
+
 		if (update_time_ >= 0.2){
-			board_.Update_(Direction::RIGHT);
+			board_.Update_(current_dir_);
 			DrawBoard_();
 			update_time_ = 0;
 		}
